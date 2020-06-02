5607d3e7e094698b3fa375d3d52680005e3ad932
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2020-06-02 23:33:01 +0100

    Update views.py

--- G__evaluation/views.py
+++ G__evaluation/views.py
@@ -47,85 +47,128 @@ def division(request):
 	nb_cf = All_Conf.count()
 	nb_evl = All_Eval.count()
 	nb_cm = All_Comite.count()
-	nb_acl = All_Artcl.count()
+	#nb_acl = All_Artcl.count()
 
 	list_of_evl = list(All_Eval)
-	list_articl = list(All_Artcl)
+	#list_articl = list(All_Artcl)
 	liste_of_commite = list(All_Comite)
 	
 
-# ++++++++++++++++++++++++++++++++++++++++++++++
-	Choisi_par_3 = Pair.objects.all()
-	List_off_Choisi_par_3=list(Choisi_par_3)
-	List_off_Artcl_Choix = []
 
-
-	for a in range(len(List_off_Choisi_par_3)):         # hatite ga3 li khayroohom fi liste 
-		List_off_Artcl_Choix.append(List_off_Choisi_par_3[a].arti)
-		
 
 	print('')
 	print('///////////////*** les article par comite apre mth 1 + 2 ***/////////////////////')
 	print('')
 
 	for Conf in All_Conf:
-		for j in range(nb_cm):
+		#for j in range(nb_cm):
 			
-			if Conf.Commite == liste_of_commite[j]:
+			#if Conf.Commite == liste_of_commite[j]:
 				
 				#+++++++++++++++++++++++++++++++
-				print('')
-				print('////////*** Aplle de 3eme methode de choix ***/////')
-				print('')
-
-				Destribute(request,liste_of_commite[j].pk) #hadi  bach n3yte distrubue w nmdlhom par mth 3
+		print('')
+		print('////////*** Aplle de 3eme methode de choix ***/////')
+		print('')
 
 
-				evl_in_cm = liste_of_commite[j].evaluteur_list.all()
-				list_of_evl_iN_cm = list(evl_in_cm)   # hna nkhdeme dakhel fi kol commite b index t3 chaque eval
-				Nbr_Artcl_in_Cm = 0
-				Num_eval_in_Cm = 0
-				
+		# ++++++++++++++++++++++++++++++++++++++++++++++
+		Choisi_par_3 = Pair.objects.filter(comit = Conf.Commite)
+		List_off_Choisi_par_3=list(Choisi_par_3)
 
-				print('')
-				print('////*** Division __Commite > ',liste_of_commite[j] ,' ***////')
-				print('')
 
-															 
-				Nbr_eval_in_Cm = len(list_of_evl_iN_cm)         # 4  evaluateurs dans chaque comite
-				# si il n ya pas d'evaluateur !! on sort
-				for art in range(nb_acl):     
 
-					if  list_articl[art] not in  List_off_Artcl_Choix :     # le cas de pas choisi donc :   
+		#evl_in_cm = liste_of_commite[j].evaluteur_list.all()
+		evl_in_cm = Conf.Commite.evaluteur_list.all()
+		list_of_evl_iN_cm = list(evl_in_cm)   # hna nkhdeme dakhel fi kol commite b index t3 chaque eval
+		Nbr_Artcl_in_Cm = 0
+		Num_eval_in_Cm = 0
 
-						if list_articl[art].Conferance == Conf:
-							Nbr_Artcl_in_Cm = Nbr_Artcl_in_Cm + 1
+	 #####################################
+	 ##################################
+	 #############################
+
+
 	
-							if Num_eval_in_Cm == Nbr_eval_in_Cm :  
-								Num_eval_in_Cm = 0
-						
-							if Num_eval_in_Cm != Nbr_eval_in_Cm :
-								list_of_evl_iN_cm[Num_eval_in_Cm].artcl_a_corrige.add(list_articl[art])
-								print('article ',list_articl[art].name ,'distribué a ',list_of_evl_iN_cm[Num_eval_in_Cm])
-
-								Num_eval_in_Cm = Num_eval_in_Cm + 1
+		qs = Article.objects.filter(Conferance = Conf)
+		list_articl = list(qs)
 
 
-					else: #cas t3 li khayro c bn ta3tihomlhom 
-
-						# hada zdetehe bch tmdelna nmbr article f cm nichan maybdach yzidena kol khatera t3 li khyrhom
-						if list_articl[art].Conferance == Conf:
-							Nbr_Artcl_in_Cm = Nbr_Artcl_in_Cm + 1 
-
-						for a in range(len(List_off_Choisi_par_3)):
-								if ((List_off_Choisi_par_3[a].arti==list_articl[art])
-															 and (List_off_Choisi_par_3[a].comit== liste_of_commite[j])):
-
-									List_off_Choisi_par_3[a].evalu.artcl_a_corrige.add(List_off_Choisi_par_3[a].arti)
+		for a in evl_in_cm :
+			a.count_pair_temporaire = 0
+			a.save()
 
 
-				print('Chaque Confrence   >>>>>>>>>> Title = ',Conf.title ,' Nbr_arcl  = :', Nbr_Artcl_in_Cm ,'Et Nbr_eval =: ', Nbr_eval_in_Cm)	
+		clean = Choosing_map.objects.all() 
+		for one in clean :
+			one.satis = 0
+			one.save()
+
+		ar_num = qs.count()
+		ev_nu = evl_in_cm.count()
+
+		limit = ar_num // ev_nu
+
+		if  ar_num % ev_nu != 0 :
+		    limit += 1
+
+		#'EV = evals.filter(count_pair_temporaire__lte = limit )
+
+		##############################################################################
+		##############################################################################
+        ##############################################################################
+
+	
+
+		Destribute(request,Conf.Commite.pk,limit) #hadi  bach n3yte distrubue w nmdlhom par mth 
 		
+		
+
+		print('')
+		print('////*** Division __Commite > ',Conf.Commite ,' ***////')
+		print('')
+
+													 
+		Nbr_eval_in_Cm = len(list_of_evl_iN_cm)-1         # 4  evaluateurs dans chaque comite
+		# si il n ya pas d'evaluateur !! on sort
+
+	###########################  1. distribution article choisi    ##########""################"
+
+		for a in range(len(List_off_Choisi_par_3)):
+
+			List_off_Choisi_par_3[a].evalu.artcl_a_corrige.add(List_off_Choisi_par_3[a].arti)
+	#############################################################################################""
+
+
+		# list_articl = sub(list_articl, List_off_Choisi_par_3) --> article non choisi 
+		nb_acl = len(list_articl)
+
+		for art in range(nb_acl):    # article de comite non choisit  
+
+			give = False
+
+			while Num_eval_in_Cm != Nbr_eval_in_Cm & not give :
+				if list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire < limit
+				    list_of_evl_iN_cm[Num_eval_in_Cm].artcl_a_corrige.add(list_articl[art]) 
+				    give = True
+				    list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire = list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire + 1
+				    print('article ',list_articl[art].name ,'distribué a ',list_of_evl_iN_cm[Num_eval_in_Cm])
+
+		        Num_eval_in_Cm = Num_eval_in_Cm + 1
+					
+			    if Num_eval_in_Cm == Nbr_eval_in_Cm
+				    Num_eval_in_Cm = 0
+
+			#else: #cas t3 li khayro c bn ta3tihomlhom 
+
+				# hada zdetehe bch tmdelna nmbr article f cm nichan maybdach yzidena kol khatera t3 li khyrhom
+				 #if list_articl[art].Conferance == Conf:
+				 #  Nbr_Artcl_in_Cm = Nbr_Artcl_in_Cm + 1 
+
+							 
+
+
+	print('Chaque Confrence   >>>>>>>>>> Title = ',Conf.title ,' Nbr_arcl  = :', Nbr_Artcl_in_Cm ,'Et Nbr_eval =: ', Nbr_eval_in_Cm)    
+
 	return render(request, 'Pour_Evaluaéé/mth_evluéé.html')
 	
   
@@ -139,50 +182,23 @@ from G__evaluation.models import Pair,Choosing_map
 
 
 @login_required
-def Destribute(request , pk):
-	conf = Conferance.objects.get(Commite = pk)
-	comit = Commite.objects.get( pk = pk )
-	evals = comit.evaluteur_list.all() 
-	qs = Article.objects.filter(Conferance = conf)
-
-	##############################################################################
-	##############################################################################
-	##############################################################################
-
-	for a in evals :
-		a.count_pair_temporaire = 0
-		a.save()
-
-	clean = Choosing_map.objects.all() 
-	for one in clean :
-		one.satis = 0
-		one.save()
-
-	ar_num = qs.count()
-	ev_nu = evals.count()
-
-	limit = ar_num // ev_nu
-
-	if  ar_num % ev_nu != 0 :
-		limit += 1
-
-	#'EV = evals.filter(count_pair_temporaire__lte = limit )
-
-	##############################################################################
-	##############################################################################
-	##############################################################################
-
+def Destribute(request , pk, limit):
+	
 
 	Choix_auncien = Pair.objects.filter(comit=pk)
 	for choix in Choix_auncien :
 		choix.delete()                  #pour evite les auncien choix et ecrase tjr active avec nvvv
-		                                 #  w tani bch ykhdem b likhyrehom mchi yg3ode il ybdel 
+										 #  w tani bch ykhdem b likhyrehom mchi yg3ode il ybdel 
+
+
+	
+	comit = Commite.objects.get( pk = pk )
+	evals = comit.evaluteur_list.all() 
+	
 
 
 
-
-
-   				
+				
 
 
 
@@ -236,9 +252,9 @@ def Destribute(request , pk):
 
 		while True :
 
-	         ######################################################################################
-	          ######################################################################################     arrange the "choosing_maps" that match to "EV" and retreiving their "articles"
-	           ######################################################################################
+			 ######################################################################################
+			  ######################################################################################     arrange the "choosing_maps" that match to "EV" and retreiving their "articles"
+			   ######################################################################################
 			hell = Choosing_map.objects.none()
 
 			for each in EV :
@@ -330,7 +346,7 @@ def Destribute(request , pk):
 
 			 ##############################################################################
 			 ##############################################################################  adjust the size of "EV" by making it smaller whenever an ev exceeds the set limit 
-			 ##############################################################################	 using "count_pair_temporaire"
+			 ##############################################################################  using "count_pair_temporaire"
 
 
 			for S in EV :
@@ -358,9 +374,7 @@ def Destribute(request , pk):
  ##############################################################################
 
 
-	for a in evals :
-		a.count_pair_temporaire = 0
-		a.save()
+	
 
 
 
