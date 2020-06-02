20a83b8a1535e9fcaa27c9097aacf81c6945b6f3
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2020-06-02 23:36:18 +0100

    Update models.py

--- mybasic_app/models.py
+++ mybasic_app/models.py
@@ -1,387 +1,179 @@
-from django.shortcuts import render,redirect
-from mybasic_app.models import User
-
-from mybasic_app.decorators import staff_member_required
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
-from mybasic_app.models import (User, Evaluateur, Chercheur,
-								Commite, Conferance, Article, Topic)
-from mybasic_app.decorators import evaluteur_required
+from django.db import models
+from  django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+from django.conf import settings
+from  django.contrib.auth.models import AbstractUser
+from PIL import Image
 
 
 
-# hadi hiyaa li rahi t9l3naaa 
+class User(AbstractUser):
+    is_chercheur = models.BooleanField(default=False)
+    is_evaluteur = models.BooleanField(default=False)
 
-@login_required
-def Update(request):
-	if request.user.is_authenticated:
-		if request.user.is_evaluteur:
-			division(request)
-			return redirect('evaluteur:dashboardeval')
-
-		if request.user.is_superuser:
-			division(request)
-			return redirect('/MakaniAdmin')
-
-	else:
-		return render(request, 'registration/login.html')
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_on']
 
 
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    Organization = models.CharField(max_length=100)
+    Phone  = models.CharField(max_length=20,unique=True)
+    City = models.CharField(max_length=100)
+    Post_code = models.CharField(max_length=20)
+    Country = models.CharField(blank=False,max_length=100)
+    Address_line_principale = models.CharField(max_length=200,blank=True)
+    Address_line = models.CharField(max_length=200,blank=True)
+    profile_pict = models.ImageField(upload_to='profile_piccs',blank=True
+        ,default='photodflt.png')
+    site_Personnel = models.URLField(blank=True)
 
 
-@login_required
-def division(request):
+    def get_absolute_url(self):
+        return reverse('EditeProfile', kwargs={'pk': self.pk})
+
+
+###############
+
+class Evaluateur(models.Model):
+    #is_evaluteur = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    email = models.EmailField(unique=True)
+    Code_Eval = models.CharField(max_length=30)
+    artcl_a_corrige = models.ManyToManyField('Article',null=True, blank=True)
    
-	All_Conf = Conferance.objects.all()
-	All_Eval = Evaluateur.objects.all()
-	All_Comite = Commite.objects.all()
-	All_Artcl = Article.objects.all()             #obj >>>>>   queryset  >>>> liste bch nkhdeme bien 
 
+    def __str__(self):
+        return  ' Le Evaluteur :' + self.user.username + ' |  | ' + ' et Sont email : ' + self.user.email
 
-	#+++++++++++++++++++++++++++++++++ evite problem de duplication et ecrasment ::
-	for evll in All_Eval:
-		evll.artcl_a_corrige.clear()
-		
+    def get_absolute_url(self):
+        return reverse('editEval', kwargs={'pk':self.id})
 
-	nb_cf = All_Conf.count()
-	nb_evl = All_Eval.count()
-	nb_cm = All_Comite.count()
-	#nb_acl = All_Artcl.count()
 
-	list_of_evl = list(All_Eval)
-	#list_articl = list(All_Artcl)
-	liste_of_commite = list(All_Comite)
-	
+    count_pair_temporaire = models.PositiveIntegerField(default=0)
 
+################
+class Chercheur(models.Model):
+    #is_chercheur = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return  ' Le Chercheur :' + self.user.username + ' |  | ' + 'et sont  email : ' + self.user.email
 
-	print('')
-	print('///////////////*** les article par comite apre mth 1 + 2 ***/////////////////////')
-	print('')
 
-	for Conf in All_Conf:
-		#for j in range(nb_cm):
-			
-			#if Conf.Commite == liste_of_commite[j]:
-				
-				#+++++++++++++++++++++++++++++++
-		print('')
-		print('////////*** Aplle de 3eme methode de choix ***/////')
-		print('')
+    def get_absolute_url(self):
+        return reverse('editCher', kwargs={'pk':self.id})
 
 
-		# ++++++++++++++++++++++++++++++++++++++++++++++
-		Choisi_par_3 = Pair.objects.filter(comit = Conf.Commite)
-		List_off_Choisi_par_3=list(Choisi_par_3)
 
 
 
-		#evl_in_cm = liste_of_commite[j].evaluteur_list.all()
-		evl_in_cm = Conf.Commite.evaluteur_list.all()
-		list_of_evl_iN_cm = list(evl_in_cm)   # hna nkhdeme dakhel fi kol commite b index t3 chaque eval
-		Nbr_Artcl_in_Cm = 0
-		Num_eval_in_Cm = 0
+#######""""/////////////////////////////////////////////////////////////////////////////
 
-	 #####################################
-	 ##################################
-	 #############################
 
+class Commite(models.Model):
+    # hado hna ndirohom dircte b groupe 
+    #user = models.ManyToManyField(Evaluateur, on_delete=models.CASCADE, primary_key=True)
 
-	
-		qs = Article.objects.filter(Conferance = Conf)
-		list_articl = list(qs)
+    #user = models.OneToOneField(Evaluateur ,on_delete=models.CASCADE, primary_key=True)
+    evaluteur_list = models.ManyToManyField(Evaluateur,null=True, blank=True)
+    Organizationof_Commite = models.CharField('Commite dans le theme',max_length=200)
+    autre_Info = models.CharField(max_length=200)
+    sepiceilite = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return self.sepiceilite
 
-		for a in evl_in_cm :
-			a.count_pair_temporaire = 0
-			a.save()
 
+    class Meta:  # bch tsmiha haka f partie adminstration t3k 
+        verbose_name = "Commites"
 
-		clean = Choosing_map.objects.all() 
-		for one in clean :
-			one.satis = 0
-			one.save()
 
-		ar_num = qs.count()
-		ev_nu = evl_in_cm.count()
 
-		limit = ar_num // ev_nu
+class ToDolist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE , 
+    	related_name="todolist" , null=True)
+    name = models.CharField(max_length=200)
 
-		if  ar_num % ev_nu != 0 :
-		    limit += 1
+    def __str__(self):
+    	return self.name
 
-		#'EV = evals.filter(count_pair_temporaire__lte = limit )
 
-		##############################################################################
-		##############################################################################
-        ##############################################################################
 
-	
+class Item(models.Model):
+	todolist = models.ForeignKey(ToDolist,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+	complete = models.BooleanField()
 
-		Destribute(request,Conf.Commite.pk,limit) #hadi  bach n3yte distrubue w nmdlhom par mth 
-		
-		
+	def __str__(self):
+		return self.text
 
-		print('')
-		print('////*** Division __Commite > ',Conf.Commite ,' ***////')
-		print('')
 
-													 
-		Nbr_eval_in_Cm = len(list_of_evl_iN_cm)-1         # 4  evaluateurs dans chaque comite
-		# si il n ya pas d'evaluateur !! on sort
 
-	###########################  1. distribution article choisi    ##########""################"
+#######""""/////////////////////////////////////////////////////////////////////////////
 
-		for a in range(len(List_off_Choisi_par_3)):
+class Topic(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(max_length=200, null=True)
 
-			List_off_Choisi_par_3[a].evalu.artcl_a_corrige.add(List_off_Choisi_par_3[a].arti)
-	#############################################################################################""
+    def __str__(self):
+        return self.title
 
 
-		# list_articl = sub(list_articl, List_off_Choisi_par_3) --> article non choisi 
-		nb_acl = len(list_articl)
 
-		for art in range(nb_acl):    # article de comite non choisit  
 
-			give = False
+class Conferance(models.Model):
 
-			while Num_eval_in_Cm != Nbr_eval_in_Cm & not give :
-				if list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire < limit
-				    list_of_evl_iN_cm[Num_eval_in_Cm].artcl_a_corrige.add(list_articl[art]) 
-				    give = True
-				    list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire = list_of_evl_iN_cm[Num_eval_in_Cm].count_pair_temporaire + 1
-				    print('article ',list_articl[art].name ,'distribué a ',list_of_evl_iN_cm[Num_eval_in_Cm])
+    title = models.CharField(max_length=200, null=True,unique=True)
+    email = models.EmailField(unique=True, null=True)
+    topic = models.ManyToManyField(Topic) ## many to many
+    Commite = models.ForeignKey(Commite,null=True, 
+     on_delete=models.SET_NULL)
+    link = models.URLField(blank=True)
+    start_day_sub = models.DateField(auto_now_add = False)
+    end_day_sub = models.DateField(auto_now_add = False)
+    start_day_eval = models.DateField(auto_now_add = False)
+    end_day_eval  = models.DateField(auto_now_add = False)
+    adress = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    postal_code = models.PositiveIntegerField(null=True)
+    city = models.CharField(max_length=100, null=True)
+    description = models.TextField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now=True)
+    
+       
+    class Meta:
+        ordering = ['-date_created','updated']
 
-		        Num_eval_in_Cm = Num_eval_in_Cm + 1
-					
-			    if Num_eval_in_Cm == Nbr_eval_in_Cm
-				    Num_eval_in_Cm = 0
 
-			#else: #cas t3 li khayro c bn ta3tihomlhom 
+    def __str__(self):
+        return self.title
 
-				# hada zdetehe bch tmdelna nmbr article f cm nichan maybdach yzidena kol khatera t3 li khyrhom
-				 #if list_articl[art].Conferance == Conf:
-				 #  Nbr_Artcl_in_Cm = Nbr_Artcl_in_Cm + 1 
+    def get_absolute_url(self):
+        return reverse('confEdit', kwargs={'pk':self.id})
 
-							 
 
+class Article(models.Model):
 
-	print('Chaque Confrence   >>>>>>>>>> Title = ',Conf.title ,' Nbr_arcl  = :', Nbr_Artcl_in_Cm ,'Et Nbr_eval =: ', Nbr_eval_in_Cm)    
+    name = models.CharField(max_length=200, null=True,unique=True)
+    description = RichTextField(blank=True , null= True)
+    author = models.ForeignKey(Chercheur,null=True, on_delete=models.SET_NULL)
+# ++++++++++++++++  kol artcile 3ndeh liste des evluter corrige au moin  2 +++ 
+    #reviwer_list = models.ManyToManyField(Evaluateur)
+    date_posté = models.DateTimeField(auto_now_add=True)
+    Conferance = models.ForeignKey(Conferance ,null=True,  on_delete=models.SET_NULL)
+    document = models.FileField(upload_to='documents_Chercheur/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    #updated = models.DateTimeField(auto_now=True)   
 
-	return render(request, 'Pour_Evaluaéé/mth_evluéé.html')
-	
-  
+    class Meta:
+        ordering = ['-date_posté']
 
-#///////////////////////////////////////////////////////////////////////////////////// 27/05/2020 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-##########################################################
-from django.db.models import Q
-from G__evaluation.models import Pair,Choosing_map
-
-
-
-@login_required
-def Destribute(request , pk, limit):
-	
-
-	Choix_auncien = Pair.objects.filter(comit=pk)
-	for choix in Choix_auncien :
-		choix.delete()                  #pour evite les auncien choix et ecrase tjr active avec nvvv
-										 #  w tani bch ykhdem b likhyrehom mchi yg3ode il ybdel 
-
-
-	
-	comit = Commite.objects.get( pk = pk )
-	evals = comit.evaluteur_list.all() 
-	
-
-
-
-				
-
-
-
-
-
-
-
-	##############################################################################
-	##############################################################################
-	##############################################################################
-
-	cho = Choosing_map.objects.filter(comit = comit)
-	arti= Article.objects.none()
-	EEV = Evaluateur.objects.none()
-	hell = Choosing_map.objects.none()
-
-	cho_count = cho.count()
-	for k in cho :
-
-		EEV = EEV.union(Evaluateur.objects.filter(pk = k.evalu.pk , count_pair_temporaire__lt = limit ))
-
-	EEV_count = EEV.count()
-
-	EV = EEV.filter(count_pair_temporaire__lt = limit )
-
-	EV_count = EV.count()
-
-
- ##############################################
- ############################################## for reduction por
- ##############################################
-
-	for each in EV :
-		hell = hell.union(cho.filter(evalu = each)).distinct()
-
-	hell_count = hell.count()
-
-	for k in hell :
-			art1 = k.art1
-			art2 = k.art2
-			art3 = k.art3
-		
-			arti = Article.objects.filter( Q(pk = art1.pk) | Q(pk = art2.pk) | Q(pk = art3.pk) ).distinct()
-
-
- ##############################################
- ##############################################
-
-
-	if cho :
-
-		while True :
-
-			 ######################################################################################
-			  ######################################################################################     arrange the "choosing_maps" that match to "EV" and retreiving their "articles"
-			   ######################################################################################
-			hell = Choosing_map.objects.none()
-
-			for each in EV :
-				hell = hell.union(cho.filter(evalu = each)).distinct()
-
-
-			for k in hell :
-					art1 = k.art1
-					art2 = k.art2
-					art3 = k.art3
-				
-					arti = Article.objects.filter( Q(id__in = arti) & (Q(pk = art1.pk) | Q(pk = art2.pk) | Q(pk = art3.pk)) ).distinct()
-
-
-			 ######################################################################################
-			  ######################################################################################
-			   ######################################################################################
-
-
-
-
-
-			eev = hell.order_by('satis')
-
-			indo = eev.first().satis
-			bundle = eev.filter(satis = indo)
-
-			for oz in bundle :
-
-				ero1 = oz.art1
-				ero2 = oz.art2
-				ero3 = oz.art3
-				if ero1 in arti :
-						if oz.evalu in EV.all() :
-							arti = arti.exclude(id =ero1.id)   ######################################################################################
-							pair = Pair(comit = comit , evalu = oz.evalu , arti = ero1)
-							pair.save()
-
-							this = Evaluateur.objects.get( pk =oz.evalu )
-							this.count_pair_temporaire += 1
-							this.save()
-
-							sample = Choosing_map.objects.filter(evalu = oz.evalu  , comit = comit ).first()
-							sample.satis += 3
-							sample.save()
-
-							arti = arti.exclude(id =ero1.id)
-
-							break
-
-				elif ero2 in arti : 
-						if oz.evalu in EV.all() :
-							arti = arti.exclude(id =ero2.id)   ######################################################################################
-							pair = Pair(comit = comit , evalu = oz.evalu   , arti = ero2)
-							pair.save()
-
-							this = Evaluateur.objects.get( pk =oz.evalu )
-							this.count_pair_temporaire += 1
-							this.save()
-
-							sample = Choosing_map.objects.filter(evalu = oz.evalu  , comit = comit ).first()
-							sample.satis += 2
-							sample.save()
-
-							arti = arti.exclude(id=ero2.id)
-
-							break
-
-				elif ero3 in arti :
-						if oz.evalu in EV.all() :
-							arti = arti.exclude(id =ero3.id)  ######################################################################################
-							pair = Pair(comit = comit , evalu = oz.evalu   , arti = ero3)
-							pair.save()
-
-							this = Evaluateur.objects.get( pk =oz.evalu  )
-							this.count_pair_temporaire += 1
-							this.save()
-
-							break
-
-							sample = Choosing_map.objects.filter(evalu = oz.evalu , comit = comit ).first()
-							sample.satis += 1
-							sample.save()
-
-							arti = arti.exclude(id =ero3.id)
-
-							break
-
-
-			 ##############################################################################
-			 ##############################################################################  adjust the size of "EV" by making it smaller whenever an ev exceeds the set limit 
-			 ##############################################################################  using "count_pair_temporaire"
-
-
-			for S in EV :
-
-				EEV = EEV.union(Evaluateur.objects.filter(pk = S , count_pair_temporaire__lt = limit ))
-
-			EV = EEV.filter(count_pair_temporaire__lt = limit )
-
-			 ##############################################################################
-			 ##############################################################################
-			 ##############################################################################
-
-
-
-			if not (arti and EV) :  ################################### because there is nothing like a "do while" in django , i had to do a ( while true : if condition :break )
-				break
-
-
-
-
-
-
- ##############################################################################
- ##############################################################################
- ##############################################################################
-
-
-	
-
-
-
-	clean = Choosing_map.objects.all() 
-	for one in clean :
-		one.satis = 0
-		one.save()
-
-
-	context = {}
+    def __str__(self):
+        return self.name
